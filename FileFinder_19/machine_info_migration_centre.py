61e8e2f432b91847756a79255979f48a73c755bc
KLUAAAAAA30 <152710147+KLUAAAAAA30@users.noreply.github.com>
2024-08-06 12:45:23 +0530

    Update machine_info_migration_centre.py

--- FileFinder_19/machine_info_migration_centre.py
+++ FileFinder_19/machine_info_migration_centre.py
@@ -1,13 +1,14 @@
-import pandas as pd
-import mysql.connector
-from datetime import datetime
 import os
+from datetime import datetime
+
+import mysql.connector
+import pandas as pd
 from dotenv import load_dotenv
 
 load_dotenv()
 
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_PORT = int(os.getenv("MYSQL_PORT"))
+MYSQL_HOST = os.getenv("DESKTOP-45002NM")
+MYSQL_PORT = int(os.getenv("3306"))
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 MYSQL_USER = os.getenv("MYSQL_USERNAME")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
@@ -22,24 +23,32 @@ connection = mysql.connector.connect(
     user=MYSQL_USER,
     password=MYSQL_PASSWORD
 )
+
 cursor = connection.cursor()
 
 insert_query = """
-INSERT INTO machine_info_migration_centre (name, create_time, ip, model, 
-                                         os_name, total_processor, total_memory, 
-                                         free_memory)
+INSERT INTO machine_info_migration_centre (
+    name, create_time, ip, model, os_name, 
+    total_processor, total_memory, free_memory
+) 
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 ON DUPLICATE KEY UPDATE
-    create_time=VALUES(create_time), ip=VALUES(ip), model=VALUES(model),
-    os_name=VALUES(os_name), total_processor=VALUES(total_processor), 
-    total_memory=VALUES(total_memory), free_memory=VALUES(free_memory)
+    create_time = VALUES(create_time),
+    ip = VALUES(ip), 
+    model = VALUES(model),
+    os_name = VALUES(os_name), 
+    total_processor = VALUES(total_processor),
+    total_memory = VALUES(total_memory), 
+    free_memory = VALUES(free_memory)
 """
 
 for _, row in df_assessment.iterrows():
     name = row['name']
     create_time = datetime.strptime(row['createDate'], '%d-%b-%Y').strftime('%Y-%m-%d')
     ip, model, os_name = row['collectedIpAddress'], row['model'], row['osName']
-    total_processor, total_memory, free_memory = row['processorCount'], row['memoryInMb'], row['driveTotalFreeInGb'] 
+    total_processor, total_memory, free_memory = (
+        row['processorCount'], row['memoryInMb'], row['driveTotalFreeInGb']
+    )
 
     values = (name, create_time, ip, model, os_name, 
               total_processor, total_memory, free_memory)
@@ -48,4 +57,5 @@ for _, row in df_assessment.iterrows():
 connection.commit()
 cursor.close()
 connection.close()
+
 print("Data migration completed successfully!")
