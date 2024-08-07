b256485265ed1bf76e04bd7c9ab333f2d03ef7de
KLUAAAAAA30 <152710147+KLUAAAAAA30@users.noreply.github.com>
2024-08-07 16:00:14 +0530

    Update machine_info_migration_centre.py

--- FileFinder_19/machine_info_migration_centre.py
+++ FileFinder_19/machine_info_migration_centre.py
@@ -25,25 +25,25 @@ connection = mysql.connector.connect(
 cursor = connection.cursor()
 
 insert_query = """
-INSERT INTO machine_info_migration_centre (name, create_time, ip, model, 
-                                           os_name, total_processor, total_memory, 
-                                           free_memory)
+INSERT INTO machine_info_migration_centre (name, create_time, ip, model, 
+                                         os_name, total_processor, total_memory, 
+                                         free_memory)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 ON DUPLICATE KEY UPDATE
-    create_time=VALUES(create_time), ip=VALUES(ip), model=VALUES(model),
-    os_name=VALUES(os_name), total_processor=VALUES(total_processor), 
-    total_memory=VALUES(total_memory), free_memory=VALUES(free_memory)
+    create_time=VALUES(create_time), ip=VALUES(ip), model=VALUES(model),
+    os_name=VALUES(os_name), total_processor=VALUES(total_processor), 
+    total_memory=VALUES(total_memory), free_memory=VALUES(free_memory)
 """
 
 for _, row in df_assessment.iterrows():
-    name = row['name']
-    create_time = datetime.strptime(row['createDate'], '%d-%b-%Y').strftime('%Y-%m-%d')
-    ip, model, os_name = row['collectedIpAddress'], row['model'], row['osName']
-    total_processor, total_memory, free_memory = row['processorCount'], row['memoryInMb'], row['driveTotalFreeInGb']
+        name = row['name']
+        create_time = datetime.strptime(row['createDate'], '%d-%b-%Y').strftime('%Y-%m-%d')
+        ip, model, os_name = row['collectedIpAddress'], row['model'], row['osName']
+        total_processor, total_memory, free_memory = row['processorCount'], row['memoryInMb'], row['driveTotalFreeInGb'] 
 
-    values = (name, create_time, ip, model, os_name, 
-              total_processor, total_memory, free_memory)
-    cursor.execute(insert_query, values)
+        values = (name, create_time, ip, model, os_name, 
+                  total_processor, total_memory, free_memory)
+        cursor.execute(insert_query, values)
 
 connection.commit()
 cursor.close()
